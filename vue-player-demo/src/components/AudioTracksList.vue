6ccf216e36f740da2569da26310dd45d67095495
Elvis Ibarra <ielvisd@gmail.com>
2023-02-22 14:28:22 -0800

    date selector works

--- vue-player-demo/src/components/AudioTracksList.vue
+++ vue-player-demo/src/components/AudioTracksList.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-    <p mt-4 v-if="!currentTrack">The current tracks are loading, please wait</p>
+  <div min-h-screen>
+    <div class="q-pa-md mx-auto" style="max-width: 300px">
+      <div class="q-gutter-md">
+        <q-select v-model="dateModel" :options="options"  :display-value="`${dateModel ? dateModel.label : '*none*'}`"
+        />
+      </div>
+    </div>
 
     <AudioPlayer
       v-if="currentTrack"
-      :track="currentTrack"
-      mt-4
       @loadedmetadata="playTrack"
+      mt-4
       @play="checkIsPlaying"
       @pause="checkIsPlaying"
-      ref="audioPlayer"
+      ref="bitcoinAudioPlayer"
       :option="{
         src: currentTrackLocation,
         title: currentTrack.name,
@@ -17,12 +21,11 @@
       }"
     />
 
-    <p>{{ isPlaying }}</p>
+    <p v-if="!tracks.length">No tracks sold during selected timeframe.</p>
 
     <audio-track
       @setCurrentTrack="loadAndOrPlayPauseCurrentTrack"
       :track="track"
-      :currentTrack="currentTrack"
       :isPlaying="isPlaying"
       v-for="track in tracks"
       :key="track.origin"
@@ -40,34 +43,88 @@ import 'vue3-audio-player/dist/style.css';
 const currentTrack = ref<Track | null>(null);
 const currentTrackLocation = ref('');
 const currentTrackImageLocation = ref('');
-const audioPlayer = ref(AudioPlayer);
+const bitcoinAudioPlayer = ref(AudioPlayer);
 const isPlaying = ref(false);
 
+const emit = defineEmits<{
+  (e: 'setNewDate', newDateModel: object): void
+}>()
+
+const dateModel= ref( {
+    label: 'Last 7 days',
+    value: '7d'
+  })
+const options = [
+  {
+    label: 'Last 24 hours',
+    value: '24h'
+  },
+  {
+    label: 'Last 7 days',
+    value: '7d'
+  },
+  {
+    label: 'Last 30 days',
+    value: '30d'
+  },
+  {
+    label: 'All time',
+    value: 'total'
+  }
+]
+
+// This function will be called when a new date is selected
+watch(dateModel, (newDateModel) => {
+  console.log('model', dateModel, newDateModel)
+  emit('setNewDate', newDateModel)
+})
+
 const playTrack = () => {
-  audioPlayer?.value?.play();
+  bitcoinAudioPlayer?.value?.play();
 };
 
 const checkIsPlaying = () => {
-  console.log('checkIsPlaying', audioPlayer.value.isPlaying)
-  audioPlayer.value.isPlaying ? (isPlaying.value = true) : (isPlaying.value = false);
+  console.log('checkIsPlaying', bitcoinAudioPlayer.value.isPlaying)
+  bitcoinAudioPlayer.value.isPlaying ? (isPlaying.value = true) : (isPlaying.value = false);
 }
+
+// Maybe just a load here
+const loadCurrentTrack = (track: Track) => {
+  console.log('track is', track)
+  // Handle no sales error
+  if(!track) {
+    currentTrack.value = {
+      name: 'No sales in current range',
+      origin: ''
+    },
+    currentTrackLocation.value = '',
+    currentTrackImageLocation.value = 'https://picsum.photos/200';
+    return
+  }
+
+  currentTrack.value = track;
+  currentTrackImageLocation.value = `https://berry2.relayx.com/${track.image}`;
+  const currentTrackLocationFromOrigin = currentTrack.value.origin.replace(
+    /.$/,
+    '2'
+  );
+  currentTrackLocation.value = `https://staging-backend.relayx.com/api/berry/${currentTrackLocationFromOrigin}`;
+};
 
 // This will load a track if it's not already loaded, and play/pause it if it is
 const loadAndOrPlayPauseCurrentTrack = (track: Track) => {
-  if (currentTrack.value?.origin === track.origin) {
-    audioPlayer.value.isPlaying
-      ? audioPlayer.value.pause()
-      : audioPlayer.value.play();
-  } else {
-    currentTrack.value = track;
-    currentTrackImageLocation.value = `https://berry2.relayx.com/${track.image}`;
+  console.log('track is', track)
+  loadCurrentTrack(track);
+  console.log('currentTrack is', currentTrack.value)
+  console.log('bitcoinAudioPlayer is', bitcoinAudioPlayer?.value, currentTrack.value?.origin === track.origin && bitcoinAudioPlayer.value.currentTime > 0)
+  console.log('currentTrack.value?.origin === track.origin', currentTrack.value?.origin === track.origin)
+  console.log(currentTrack.value?.origin === track.origin && bitcoinAudioPlayer.value.play !== undefined)
 
-    const currentTrackLocationFromOrigin = currentTrack.value.origin.replace(
-      /.$/,
-      '2'
-    );
-
-    currentTrackLocation.value = `https://staging-backend.relayx.com/api/berry/${currentTrackLocationFromOrigin}`;
+  console.log(currentTrack.value?.origin === track.origin && bitcoinAudioPlayer?.value)
+  if (currentTrack.value?.origin === track.origin && typeof bitcoinAudioPlayer.value.play === 'function')  {
+    bitcoinAudioPlayer.value.isPlaying
+      ? bitcoinAudioPlayer.value.pause()
+      : bitcoinAudioPlayer.value.play();
   }
 };
 
@@ -78,7 +135,7 @@ const props = withDefaults(defineProps<Props>(), {
   tracks: () => [],
 });
 
-watch(props, () => {
-  loadAndOrPlayPauseCurrentTrack(props.tracks[0]);
+watch(currentTrack, (newTrack) => {
+  loadAndOrPlayPauseCurrentTrack(newTrack ? newTrack : {name: 'No sales in current range', origin: ''});
 });
 </script>
--- vue-player-demo/src/pages/IndexPage.vue
+++ vue-player-demo/src/pages/IndexPage.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <audio-tracks-list
+      @setNewDate="getAudioRankings"
       title="Example component"
       active
       :tracks="audioNFTs"
@@ -15,13 +16,15 @@ import { ref, watch, onBeforeMount } from 'vue';
 import { useQuery } from '@vue/apollo-composable';
 import gql from 'graphql-tag';
 
+
 // TODO: type the audioNFTs array
 const audioNFTs = ref<Track[]>([]);
-const getAudioRankings = async () => {
+const getAudioRankings = async (dateFilter: object) => {
+  console.log('getAudioRankings called', dateFilter)
   const { result } = useQuery(gql`
     query Ranks{
       ranks(
-      filter: "total",
+      filter: "${dateFilter.value}",
       category: "AUDIO",
       search: "") {
         name,
@@ -40,15 +43,18 @@ const getAudioRankings = async () => {
       }
     }
   `);
+  audioNFTs.value = result.value?.ranks;
 
-  watch(result, (value) => {
-      audioNFTs.value = value?.ranks;
-    }
-  );
+  watch(result, (newResult) => {
+    audioNFTs.value = newResult.ranks;
+  });
 };
 
 // Get audio NFTs before mount
 onBeforeMount(() => {
-  getAudioRankings();
+  getAudioRankings({
+    label: 'Last 7 days',
+    value: '7d'
+  });
 });
 </script>
